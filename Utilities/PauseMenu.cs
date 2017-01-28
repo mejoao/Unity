71a517d12bce9380a33c5cd798867bd3bb65f335
Roberto Bianchini <roczbianchini@gmail.com>
2017-01-28 00:26:21 -0200

    Correct format

--- Utilities/PauseMenu.cs
+++ Utilities/PauseMenu.cs
@@ -7,61 +7,57 @@ using UnityEditor;
 
 public class PauseMenu : MonoBehaviour {
 
-  public  KeyCode pauseKey = KeyCode.Escape;
+	public  KeyCode pauseKey = KeyCode.Escape;
 	public  string  levelToLoad;
 	private bool    pause;
 
     public void Start() {
-        pause          = false;
+		pause          = false;
         Cursor.visible = false;
-
     }
 
 
   public void PauseGame() {
-    Cursor.visible       = true;
-  	Time.timeScale       = 0;
-  	Time.fixedDeltaTime  = 0;
-
+		Cursor.visible       = true;
+		Time.timeScale       = 0;
+		Time.fixedDeltaTime  = 0;
   		// Pause the other objects if needed
   }
 
 
   public void UnPauseGame() {
-    Cursor.visible       = false;
-  	Time.timeScale       = 1;
-  	Time.fixedDeltaTime  = 1;
-
-  	// Pause the other objects if needed
+		Cursor.visible       = false;
+		Time.timeScale       = 1;
+		Time.fixedDeltaTime  = 1;
+  		// UnPause the other objects if needed
   }
 
   public void ReloadGame() {
-  	paused         = false;
-    Cursor.visible = false;
-    Time.timeScale = 1;
-    SceneManager.LoadScene(levelToLoad);
+		paused         = false;
+		Cursor.visible = false;
+		Time.timeScale = 1;
+		SceneManager.LoadScene(levelToLoad);
   }
 
 
   public void QuitGame() {
-   	Time.timeScale      = 1;
-   	Time.fixedDeltaTime = 1;
-   	#if UNITY_EDITOR
-   	  EditorApplication.isPlaying = false;
-   	#endif
-     	Application.Quit();
-  	#endif
+		Time.timeScale      = 1;
+   		Time.fixedDeltaTime = 1;
+   		#if UNITY_EDITOR
+   	  		EditorApplication.isPlaying = false;
+   		#endif
+     		Application.Quit();
+  		#endif
   }
 
 
   public void Update() {
-    if (Input.GetKeyUp(pauseKey)) {
-      pause = !pause;
-      if (pause) {
-  	   PauseGame();
-      } else {
-        UnPauseGame();
-      }
+		if (Input.GetKeyUp(pauseKey)) {
+      		pause = !pause;
+      		if (pause)
+  	   			PauseGame();
+      		else
+        		UnPauseGame();
     }
   }
 }
